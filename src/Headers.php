613907d4e9752fa477c37bdc396467565cdd424e
vvthanatos <vvthanatos@gmail.com>
2019-09-21 11:23:34 +0300

    auth header improved

--- src/Headers.php
+++ src/Headers.php
@@ -8,9 +8,23 @@ class Headers
      */
     private $headers;
 
+    /**
+     * @var array
+     */
+    private $httpHeaders = [];
+
     public function __construct()
     {
         $this->headers = $_SERVER;
+
+        if (\headers_sent()) {
+            $httpHeaders = \getallheaders();
+
+            if ($httpHeaders !== false) {
+                foreach ($httpHeaders as $key => $value)
+                    $this->httpHeaders[strtoupper($key)] = $value;
+            }
+        }
     }
 
     /**
@@ -30,6 +44,10 @@ class Headers
             return $this->headers['AUTHORIZATION'];
         } elseif (array_key_exists('HTTP_AUTHORIZATION', $this->headers)) {
             return $this->headers['HTTP_AUTHORIZATION'];
+        } elseif (array_key_exists('AUTHORIZATION', $this->headers)) {
+            return $this->headers['AUTHORIZATION'];
+        } elseif (array_key_exists('HTTP_AUTHORIZATION', $this->headers)) {
+            return $this->headers['AUTHORIZATION'];
         } else {
             return null;
         }
